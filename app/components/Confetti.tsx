ebf3370920fe773a8f7faaf066404fee3afdf0b0
Bojana Borković <bojana.borkovic@outlook.com>
2025-07-04 16:31:11 +0200

    fixed build errors

--- app/components/Confetti.tsx
+++ app/components/Confetti.tsx
@@ -7,40 +7,26 @@ interface ConfettiProps {
   trigger: boolean;
 }
 
+// Move this function outside of useEffect
+function randomInRange(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
 export default function Confetti({ trigger }: ConfettiProps) {
   useEffect(() => {
     if (trigger) {
-      // Create a confetti cannon
-      const duration = 3 * 1000;
-      const animationEnd = Date.now() + duration;
       const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-      function randomInRange(min: number, max: number) {
-        return Math.random() * (max - min) + min;
+      for (let i = 0; i < 5; i++) {
+        confetti({
+          ...defaults,
+          particleCount: 50,
+          origin: {
+            x: randomInRange(0.1, 0.9),
+            y: randomInRange(0.1, 0.3)
+          }
+        });
       }
-
-      const interval = setInterval(function() {
-        const timeLeft = animationEnd - Date.now();
-
-        if (timeLeft <= 0) {
-          return clearInterval(interval);
-        }
-
-        const particleCount = 50 * (timeLeft / duration);
-
-        // since particles fall down, start a bit higher than random
-        confetti(Object.assign({}, defaults, {
-          particleCount,
-          origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }
-        }));
-        confetti(Object.assign({}, defaults, {
-          particleCount,
-          origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }
-        }));
-      }, 250);
-
-      // Clean up interval on unmount
-      return () => clearInterval(interval);
     }
   }, [trigger]);
 
